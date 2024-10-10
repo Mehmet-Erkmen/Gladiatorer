0bd8796f51719878012963c004070339794e2c86
Mehmet Erkmen <mehmet.erkmen@learnet.se>
2024-10-10 12:56:00 +0200

    Jag har forsättade med Gladiatorerna

--- Gladiatorer.py
+++ Gladiatorer.py
@@ -1,3 +1,12 @@
 import random,colorama,msvcrt
 
 print("Du är Gladiatorn Lucius, du ska slåss mot Gladiatorn Gaius")
+print("Ni ligger i en romersk arena och är omrigande av publik.")
+
+WChoice= input("Vilken vapen vill du välja(Svärd eller Spjut). Sköld kommer med.")
+
+if WChoice == "svärd":
+    print("Du har vald svärd som vapen.")
+
+elif WChoice == "spjut":
+    print("Du valde spjut som vapen.")
