27777362f6a3b756c717803c54ec02ff88a6845d
Mehmet Erkmen <mehmet.erkmen@learnet.se>
2024-10-15 10:33:48 +0200

    Jag gjorde ett while loop

--- Gladiatorer.py
+++ Gladiatorer.py
@@ -1,4 +1,4 @@
-import random,colorama,msvcrt
+import random,msvcrt
 
 print("Du är Gladiatorn Lucius, du ska slåss mot Gladiatorn Gaius")
 print("Ni ligger i en romersk arena och är omrigande av publik.")
@@ -10,3 +10,23 @@ if WChoice == "svärd":
 
 elif WChoice == "spjut":
     print("Du valde spjut som vapen.")
+
+move = ["spark", "slag", "kast"]
+
+print("Vi börjar med första rundan")
+rund1 = 1
+while rund1:
+    luciusmove=input("Vilket väljer du: slag, spark eller kast")
+
+    slagdamage = random.randint(1,10) 
+    slagchance = random.randint(1,10)
+    sparkdamage = random.randint(1,10)
+    sparkchance = random.randint(1,10) 
+    kastdamage = random.randint(1,10)
+    kastchance = random.randint(1,10)
+    gaiusmove= random.choice(move)
+
+    print("Du väljde", luciusmove, "och Gaius väljde", gaiusmove)
+
+    if luciusmove == "spark":
+        
