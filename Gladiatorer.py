ce6024cab97b422ac1e996de31717b1d1578bfb2
Mehmet-Erkmen <mehmet.erkmen@elev.ga.ntig.se>
2024-10-17 13:02:26 +0200

    Add files via upload

--- Gladiatorer.py
+++ Gladiatorer.py
@@ -11,22 +11,48 @@ if WChoice == "svärd":
 elif WChoice == "spjut":
     print("Du valde spjut som vapen.")
 
-move = ["spark", "slag", "kast"]
+svärdmove = ["hacka", "stabba"]
+spjutmove = ["kasta spjut", "spjut droppe"]
+luciushp = 100
+gaiushp = 100
+gaiusW = random.choices(svärdmove, spjutmove)
 
 print("Vi börjar med första rundan")
 rund1 = 1
 while rund1:
-    luciusmove=input("Vilket väljer du: slag, spark eller kast")
+    if WChoice == "svärd":
+        luciusmove = input("Vilken väljer du: Hacka eller stabba? ")
+    elif WChoice == "spjut":
+        luciusmove = input("Vilken väljer du: Kasta spjut eller spjut droppe? ")
+    WChoice = WChoice.lower()
 
-    slagdamage = random.randint(1,10) 
-    slagchance = random.randint(1,10)
-    sparkdamage = random.randint(1,10)
-    sparkchance = random.randint(1,10) 
-    kastdamage = random.randint(1,10)
+    hackadamage = random.randint(7,10) 
+    hackachance = random.randint(1,10)
+    stabbdamage = random.randint(4,8)
+    stabbchance = random.randint(1,10) 
+    kastdamage = random.randint(3,7)
     kastchance = random.randint(1,10)
-    gaiusmove= random.choice(move)
+    dropdamage = random.randint(4,7)
+    dropchance = random.randint(1,10)
+    gaiusmove= random.choice(gaiusW)
 
     print("Du väljde", luciusmove, "och Gaius väljde", gaiusmove)
 
-    if luciusmove == "spark":
+    if luciusmove == "stabba":
+        if stabbchance >= 6:
+            gaiushp - stabbdamage
+    elif luciusmove == "hacka":
+        if hackadamage >= 4:
+            gaiushp - hackadamage
+    elif luciusmove == "kasta spjut":
+        if kastchance >=8:
+            gaiushp - kastdamage
+    elif luciusmove == "spjut droppe":
+        dropchance >= 6
+
+    elif luciusmove == gaiusmove:
+        print("Ni har valt samma")
+        continue
+    
         
+
