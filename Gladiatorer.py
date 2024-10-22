72c67ffcf9e608ad348138b0f44fe86d08f055b7
Mehmet-Erkmen <mehmet.erkmen@elev.ga.ntig.se>
2024-10-22 10:18:45 +0200

    Add files via upload
    Jag gjorde fler vapen för motståndaren

--- Gladiatorer.py
+++ Gladiatorer.py
@@ -1,30 +1,39 @@
+# Steg 1: Undersök om fienden väljer att använda vapnet eller istället slår eller sparkar
+# Steg 2: Om den väljer vapnet, bestäm vilken attack med vapnet den ska göra
+# Steg 3: Se om det är en träff eller en miss
+# Steg 4: Om det är en miss blir det spelarens tur att anfalla
+# Steg 5: Om det är en träff ska spelaren ta skada
+# Steg 6: berätta för spelaren vad som händer
+
+
 import random,msvcrt
 
 print("Du är Gladiatorn Lucius, du ska slåss mot Gladiatorn Gaius")
 print("Ni ligger i en romersk arena och är omrigande av publik.")
 
-WChoice= input("Vilken vapen vill du välja(Svärd eller Spjut). Sköld kommer med.")
+wchoice= input("Vilken vapen vill du välja(Svärd eller Spjut). Sköld kommer med.").lower()
 
-if WChoice == "svärd":
+if wchoice == "svärd":
     print("Du har vald svärd som vapen.")
-
-elif WChoice == "spjut":
+elif wchoice == "spjut":
     print("Du valde spjut som vapen.")
 
+weapons = ["svärd", "spjut"]
 svärdmove = ["hacka", "stabba"]
 spjutmove = ["kasta spjut", "spjut droppe"]
 luciushp = 100
 gaiushp = 100
-gaiusW = random.choices(svärdmove, spjutmove)
+gaiusW = random.choice(weapons)
+print(gaiusW)
 
 print("Vi börjar med första rundan")
 rund1 = 1
 while rund1:
-    if WChoice == "svärd":
+    if wchoice == "svärd":
         luciusmove = input("Vilken väljer du: Hacka eller stabba? ")
-    elif WChoice == "spjut":
+    elif wchoice == "spjut":
         luciusmove = input("Vilken väljer du: Kasta spjut eller spjut droppe? ")
-    WChoice = WChoice.lower()
+    wchoice = wchoice.lower()
 
     hackadamage = random.randint(7,10) 
     hackachance = random.randint(1,10)
