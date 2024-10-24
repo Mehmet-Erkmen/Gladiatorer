731c0bf222cbfafecd9332732bb566b44d39f518
Mehmet-Erkmen <mehmet.erkmen@elev.ga.ntig.se>
2024-10-24 12:21:29 +0200

    Add files via upload
    Jag fortsatt med flera vapen i spelet

--- Gladiatorer.py
+++ Gladiatorer.py
@@ -11,20 +11,29 @@ import random,msvcrt
 print("Du är Gladiatorn Lucius, du ska slåss mot Gladiatorn Gaius")
 print("Ni ligger i en romersk arena och är omrigande av publik.")
 
+weapons = ["svärd", "spjut"]
+svärdmove = ["hacka", "stabba"]
+spjutmove = ["kasta spjut", "spjut droppe"]
+luciushp = 100
+gaiushp = 100
+
 wchoice= input("Vilken vapen vill du välja(Svärd eller Spjut). Sköld kommer med.").lower()
+
+gaiusw = random.choice(weapons)
+if gaiusw == "svärd":
+    gaiusmove= random.choice(svärdmove)
+elif gaiusw == "spjut":
+    gaiusmove= random.choice(spjutmove)
 
 if wchoice == "svärd":
     print("Du har vald svärd som vapen.")
 elif wchoice == "spjut":
     print("Du valde spjut som vapen.")
 
-weapons = ["svärd", "spjut"]
-svärdmove = ["hacka", "stabba"]
-spjutmove = ["kasta spjut", "spjut droppe"]
-luciushp = 100
-gaiushp = 100
-gaiusW = random.choice(weapons)
-print(gaiusW)
+
+
+
+
 
 print("Vi börjar med första rundan")
 rund1 = 1
@@ -34,6 +43,7 @@ while rund1:
     elif wchoice == "spjut":
         luciusmove = input("Vilken väljer du: Kasta spjut eller spjut droppe? ")
     wchoice = wchoice.lower()
+   
 
     hackadamage = random.randint(7,10) 
     hackachance = random.randint(1,10)
@@ -43,7 +53,7 @@ while rund1:
     kastchance = random.randint(1,10)
     dropdamage = random.randint(4,7)
     dropchance = random.randint(1,10)
-    gaiusmove= random.choice(gaiusW)
+    
 
     print("Du väljde", luciusmove, "och Gaius väljde", gaiusmove)
 
@@ -57,10 +67,25 @@ while rund1:
         if kastchance >=8:
             gaiushp - kastdamage
     elif luciusmove == "spjut droppe":
-        dropchance >= 6
+        if dropchance >= 6:
+            gaiushp - dropdamage
+        
 
     elif luciusmove == gaiusmove:
         print("Ni har valt samma")
+
+    if gaiusmove == "stabba":
+        if stabbchance >= 6:
+            luciushp - stabbdamage
+    elif gaiusmove == "hacka":
+        if hackadamage >= 4:
+            luciushp - hackadamage
+    elif gaiusmove == "kasta spjut":
+        if kastchance >=8:
+            luciushp - kastdamage
+    elif gaiusmove == "spjut droppe":
+        if dropchance >= 6:
+            luciushp - dropdamage
         continue
     
         
